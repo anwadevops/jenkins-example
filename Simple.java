07cd16b54d0ff1aa0facd081e1f1b742b0e82745
show <root@ip-172-31-95-103.ec2.internal>
2021-08-25 08:24:10 +0000

    S3

--- Simple.java
+++ Simple.java
@@ -1,5 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("This is SCM example");  
+     System.out.println("This is the last option to build the source code");
     }  
 }  
